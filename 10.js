35c496ab646322966bbead7ee756b4c455b477d9
Ulyana Gromyko <UlyanaG2001@yandex.by>
2021-05-18 23:02:32 +0300

    commit

---
+++ 10.js
@@ -0,0 +1,5 @@
+let findNeedle = (arr) =>{
+    let missing=arr.indexOf("needle")
+    console.log(`Нашел иглу в положении ${missing}`)
+    };
+    findNeedle(["bla","wekmvever","needle","bntyn","qwd","qfverv","rtbrtb"])
